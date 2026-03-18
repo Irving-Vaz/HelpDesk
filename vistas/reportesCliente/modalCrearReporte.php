079eabb5fab9019cf0401957a87c88712b0dd204
Irving-Vaz <pressivazquez@gmial.com>
2026-03-18 16:51:48 -0600

    creacion de conexion para el modal de reportes

--- vistas/reportesCliente/modalCrearReporte.php
+++ vistas/reportesCliente/modalCrearReporte.php
@@ -9,15 +9,38 @@
       </div>
       <div class="modal-body">
         <label for="idEquipo">Mis dispositivos</label>
+        <?php 
+        $idUsuario = $_SESSION['usuario']['id'];
+        $sql = "SELECT 
+                    asignacion.id_asignacion as idAsignacion,
+                    equipo.id_equipo as idEquipo,
+                    equipo.nombre as nombreEquipo
+                FROM
+                    t_asignacion AS asignacion
+                        INNER JOIN
+                    t_cat_equipo AS equipo ON asignacion.id_equipo = equipo.id_equipo
+                WHERE
+                    asignacion.id_persona = (SELECT 
+                            id_persona
+                        FROM
+                            t_usuarios
+                        WHERE
+                            id_usuario = '$idUsuario')";
+        $respuesta = mysqli_query($conexion, $sql);
+        ?>
         <select name="idEquipo" id="idEquipo" class="form-control" required>
             <option value="">Selecionar</option>
+            <?php 
+                while($mostrar = mysqli_fetch_array($respuesta)){?>
+                <option value="<?php echo $mostrar['idEquipo'];?>"><?php echo $mostrar['nombreEquipo']; ?></option>
+            <?php } ?>
         </select>
         <label for="problema">Describe tu problema</label>
         <textarea name="problema" id="problema" class="form-control" required></textarea>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button  class="btn btn-primary">Crear nuevo Reporte</button>
+        <button type="submit" class="btn btn-primary">Crear nuevo Reporte</button>
       </div>
     </div>
   </div>
--- vistas/reportesCliente/tablaReporteCliente.php
+++ vistas/reportesCliente/tablaReporteCliente.php
@@ -1,9 +1,44 @@
+<?php
+    session_start();
+    include "../../clases/Conexion.php";
+    $con = new Conexion();
+    $conexion = $con->conectar();
+    $idUsuario  =$_SESSION['usuario']['id'];
+    $contador = 1;
+    $sql = "SELECT 
+                reporte.id_reporte AS idReporte,
+                reporte.id_usuario AS idUsuario,
+                CONCAT(persona.paterno,
+                        '',
+                        persona.materno,
+                        '',
+                        persona.nombre) AS nombrePersona,
+                equipo.id_equipo AS idEquipo,
+                equipo.nombre as nombreEquipo,
+                reporte.descripcion_problema as problema,
+                reporte.estatus AS estatus,
+                reporte.solucion_problema as solucion,
+                reporte.fecha as fecha
+            FROM
+                t_reportes AS reporte
+                    INNER JOIN
+                t_usuarios AS usuario ON reporte.id_usuario = usuario.id_usuario
+                    INNER JOIN
+                t_persona AS persona ON usuario.id_persona = persona.id_persona
+                    INNER JOIN
+                t_cat_equipo AS equipo ON reporte.id_equipo = equipo.id_equipo
+                    AND reporte.id_usuario = '$idUsuario'";
+    $respuesta = mysqli_query($conexion,$sql);
+
+?>
+
+
+
+
 <table class="table table-sm dt-responsive nowrap" style="width:100%" id="tablaAsignacionDataTable" id="tablaReportesClienteDataTable">
     <thead>
         <th>#</th>
-        <th>Apellido paterno</th>
-        <th>Apellido materno</th>
-        <th>Nombre</th>
+        <th>Persona</th>
         <th>Dispositivo</th>
         <th>Fecha</th>
         <th>Descripcion</th>
@@ -12,18 +47,36 @@
         <th>Eliminar</th>
     </thead>
     <tbody>
+    <?php
+        while($mostrar = mysqli_fetch_array($respuesta)){
+    ?>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td><?php echo $contador++; ?></td>
+            <td><?php echo $mostrar['nombrePersona'];?></td>
+            <td><?php echo $mostrar['nombreEquipo'];?></td>
+            <td><?php echo $mostrar['fecha'];?></td>
+            <td><?php echo $mostrar['problema'];?></td>
+            <td><?php 
+                $estatus = $mostrar['estatus'];
+                $cadenaEstatus = '<div class="alert alert-danger" role="alert">
+                                        Abierto
+                                   </div>';
+                if ($estatus == 0) {
+                    $cadenaEstatus = '<div class="alert alert-success" role="alert">
+                                        Cerrado
+                                    </div>';
+                }
+                echo $cadenaEstatus;
+                ?>
+            </td>
+            <td><?php echo $mostrar['solucion'];?></td>
+            <td>
+                <button class="btn btn-danger btn-sm">Eliminar</button>
+            </td>
         </tr>
+    <?php
+        }
+    ?>
     </tbody>
 </table>
 
