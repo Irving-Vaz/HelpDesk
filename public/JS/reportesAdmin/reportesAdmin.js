6833ffd9d7085091878149955df0c1e35bd10158
Irving-Vaz <pressivazquez@gmial.com>
2026-03-18 18:33:10 -0600

    maquetacion en la vista reportes por el lado del admin, visualizacion de registros en la tabla y funcionalidad del boton eliminar

---
+++ public/JS/reportesAdmin/reportesAdmin.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('#tablaReporteAdminLoad').load('reportesAdmin/tablaReportesAdmin.php');
+    
+});
+
+function eliminarReporteAdmin(idReporte) {
+    Swal.fire({
+        title: "¿Estas seguro de eliminar este registro?",
+        text: "Una vez confirmada la accion, no hay recuperacion de datos!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type:"POST",
+                data:"idReporte=" + idReporte,
+                url:"../procesos/reportesCliente/eliminarReporteCliente.php",
+                success:function(respuesta){
+                    if (respuesta == 1) {
+                            $('#tablaReporteClienteLoad').load('reportesCliente/tablaReporteCliente.php');
+                            Swal.fire(":D","Eliminado con exito","success");
+                    }else{
+                            Swal.fire(":D","Eliminacion fallida" + respuesta,"error");
+                    }
+                }
+            });
+        }
+    });
+    
+    return false;
+}
--- vistas/reportes.php
+++ vistas/reportes.php
@@ -8,13 +8,19 @@
         <div class="card border-0 shadow my-5">
             <div class="card-body p-5">
                 <h1 class="fw-light">Reportes</h1>
-                <p class="lead">Content on the page will</p>
+                <p class="lead">
+                    <hr>
+                    <div id="tablaReporteAdminLoad"></div>
+                </p>
             </div>
         </div>
     </div>
     
 <?php  
     include "footer.php";
+?>
+<script src="../public/JS/reportesAdmin/reportesAdmin.js"></script>
+<?php
     }else{
         header("location:../index.html");
     }
